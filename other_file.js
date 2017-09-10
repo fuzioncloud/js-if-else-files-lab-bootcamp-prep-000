83a84ab6427ab020e554b0591322a0bf95fde167
fuzioncloud <zzaharris@fuzioncloud.com>
2017-09-10 17:10:15 +0000

    automatically backed up by learn

--- other_file.js
+++ other_file.js
@@ -1,17 +1,3 @@
 var index = require("./index.js")
 
 //console.log(index.name)
-
-if (index.name === "Susan") {
-  console.log("The name "+index.name+" is correct")
-} else {
-  name = "Susan";
-  console.log("Expected " +name+ "; Received the name "+index.name+" instead.")
-}
-
-if (index.height === 70) {
-  console.log("The correct height of "+index.height+" was returned.")
-} else {
-  height = 70;
-  console.log("Expected a height of " + height + ", instead a height of " + index.height + " was returned.")
-}

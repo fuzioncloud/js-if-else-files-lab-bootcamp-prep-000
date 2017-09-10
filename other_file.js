a4e6816db7d2b7d3b391d9f4277d2b783dbedb5f
fuzioncloud <zzaharris@fuzioncloud.com>
2017-09-10 16:41:53 +0000

    automatically backed up by learn

--- other_file.js
+++ other_file.js
@@ -1,3 +1,9 @@
 var index = require("./index.js")
 
 console.log(index.name)
+
+if (name === "Susan") {
+  console.log("The name is correct")
+} else {
+  console.log("Expected the name to be Susan")
+}
